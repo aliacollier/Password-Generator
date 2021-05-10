253c43210aedf8625ba4cd3f68f9324e38c84669
aliacollier <aliacollier15@gmail.com>
2021-05-09 21:22:54 -0500

    Added variable arrays & declarations; Prompt for password length

--- assets/js/js.js
+++ assets/js/js.js
@@ -2,24 +2,40 @@
 // Added querySelector w/ Id Event listener to generate button; Once clicked this will start prompt for password.
 document.querySelector("#generate").addEventListener("click", writePassword);
 
-// #2 create var and declaration for: passwordLength, specialCharacters, upperCase, and lowerCase.
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var specialCharacters = ['!','#', '$', '%', '&', '(', ')', '*', '+', '-', '=', '.', '/', '?', '@', '[', ']', '^', '_', '`', '{', '}', '|', '~',';', ':' ] 
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+// #2 create var and arrays for: passwordLength, specialCharacters, upperCase, and lowerCase.
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters = ['!','#', '$', '%', '&', '(', ')', '*', '+', '-', '=', '.', '/', '?', '@', '[', ']', '^', '_', '`', '{', '}', '|', '~',';', ':' ];
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var lowerCase = upperCase.toLowerCase();
+// can I do this?? (ABOVE CODE)
 
-// #3 create function & prompt for passwordLength.
-var passwordLength = prompt("How many character would you like for your password?");
+// #3 create var declarations
+ var verifyPasswordLength = '';
+ var verifySpecialCharacters;
+ var verifyUpperCase;
+ var verifyLowerCase;
 
-var passwordLength
-window.prompt();
+// #4 create function & prompt for passwordLength.
+var passwordLengthPrompt = ["How many characters would you like for you password?"]
 
-// #4 Write Loop for incorrect answer and prompt question again.
-// #5 Alert how many characters the user wants.
-// #6 Password criteria selection
-// #7 Write Loop for inccorect answers and prompt questions again until all appropriate criteria is selected
-// #8 Action for password parameters
-// #9 Empty string to be filled based on loop selecting random characters form the array
+function generatePassword() {
+  var verifyPasswordLength = prompt("How many characters would you like for your password?");
+}
+// Btn is not working...
+
+/*
+#5 Write Loop to 1st: check if the correct number of characters is selected;
+2nd: verify the select number of characters for password length.
+*/
+while(verifyPasswordLength >= 8 || verifyPasswordLength <= 100) {
+  alert("Your password must have between 8-100 characters! Please try again.");
+  var verifyPasswordLength = prompt("How many characters would you like for your password?");
+} 
+// #6 Alert how many characters the user selected.
+// #7 Password criteria selection
+// #8 Write Loop for inccorect answers and prompt questions again until all appropriate criteria is selected
+// #9 Action for password parameters
+// #10 Empty string to be filled based on loop selecting random characters form the array
 // 
 /* 
 use window.prompt()??; NO WINDOW NEEDED DEFEATS PURPOSE OF GENERATE BTN
@@ -40,4 +56,3 @@ function writePassword() {
 
   passwordText.value = password;
 }
-
