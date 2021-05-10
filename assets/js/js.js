34fcf6bb2bebf2efe245d59c1433a28a10365e31
aliacollier <aliacollier15@gmail.com>
2021-05-09 22:46:24 -0500

    Added variables & loops for different characters selected

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,72 +1,96 @@
 // Assignment code here
+alert("This works")
 // Added querySelector w/ Id Event listener to generate button; Once clicked this will start prompt for password.
 document.querySelector("#generate").addEventListener("click", writePassword);
 
 // #2 create var and arrays for: passwordLength, specialCharacters, upperCase, and lowerCase.
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialCharacters = ['!','#', '$', '%', '&', '(', ')', '*', '+', '-', '=', '.', '/', '?', '@', '[', ']', '^', '_', '`', '{', '}', '|', '~',';', ':' ];
+var specialCharacters = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '=', '.', '/', '?', '@', '[', ']', '^', '_', '`', '{', '}', '|', '~', ';', ':'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = upperCase.toLowerCase();
 // can I do this?? (ABOVE CODE)
 
 // #3 create var declarations for criteria
- var verifyPasswordLength = '';
- var verifySpecialCharacters;
- var verifyUpperCase;
- var verifyLowerCase;
- //Don't forget the verifyNumbers!!!!
-
-// #4 create function & prompt for passwordLength.
-var passwordLengthPrompt = ["How many characters would you like for you password?"]
-
-function generatePassword() {
-  var verifyPasswordLength = prompt(passwordLengthPrompt);
-}
-// Btn is not working...
-
-/*
-#5 Write Loop to 1st: check if the correct number of characters is selected;
-2nd: verify the select number of characters for password length.
-*/
-while(verifyPasswordLength >= 8 || verifyPasswordLength <= 100) {
-  alert("Your password must have between 8-100 characters! Please try again.");
-  var verifyPasswordLength = prompt("How many characters would you like for your password?");
-}
-
-// #6 Alert how many characters the user selected (Note: how do I add the verifyPasswordLength string to alert??)
-alert("Your password will be" + verifyPasswordLength + "characters long");
-
-// #7 Password criteria selection
-var verifySpecialCharacters = confirm("Click Ok if you want special characters in your password.");
-var verifyUpperCase = confirm("Click Ok if you want uppercase characters in your password.");
-var verifyLowerCase = confirm("Click Ok if you want lowercase characters in your password.");
+var verifyPasswordLength = '';
+var verifySpecialCharacters;
+var verifyUpperCase;
+var verifyLowerCase;
 //Don't forget the verifyNumbers!!!!
 
-// #8 Write Loop (while best fit!) for invalid non-selection & prompt questions again until at least 1 of criteria is selected
-while(verifySpecialCharacters === false && verifyUpperCase === false && verifyLowerCase === false &&) {
-  alert("You have to choose one of the selections!");
+// #4 create function & prompt for passwordLength.
+  /* 
+  use window.prompt()??; NO WINDOW NEEDED DEFEATS PURPOSE OF GENERATE BTN
+  if so before using window.prompt put in a var and it's name in front of the
+  window prompt to catch the data. Also look up edge cases mod.3.1.8
+  */
+
+function generatePassword() {
+  var verifyPasswordLength = prompt("How many characters would you like for you password?");
+
+  // Btn is not working...
+
+  /*
+  #5 Write Loop to 1st: check if the correct number of characters is selected;
+  2nd: verify the select number of characters for password length.
+  */
+  while (verifyPasswordLength >= 8 || verifyPasswordLength <= 100) {
+    alert("Your password must have between 8-100 characters! Please try again.");
+    var verifyPasswordLength = prompt("How many characters would you like for your password?");
+  }
+
+  // #6 Alert how many characters the user selected (Note: how do I add the verifyPasswordLength string to alert??)
+  alert("Your password will be" + verifyPasswordLength + "characters long");
+
+  // #7 Password criteria selection
   var verifySpecialCharacters = confirm("Click Ok if you want special characters in your password.");
   var verifyUpperCase = confirm("Click Ok if you want uppercase characters in your password.");
   var verifyLowerCase = confirm("Click Ok if you want lowercase characters in your password.");
   //Don't forget the verifyNumbers!!!!
 
+  // #8 Write Loop (while best fit!) for invalid non-selection & prompt questions again until at least 1 of criteria is selected
+  while (verifySpecialCharacters === false && verifyUpperCase === false && verifyLowerCase === false) {
+    alert("You have to choose one of the selections!");
+    var verifySpecialCharacters = confirm("Click Ok if you want special characters in your password.");
+    var verifyUpperCase = confirm("Click Ok if you want uppercase characters in your password.");
+    var verifyLowerCase = confirm("Click Ok if you want lowercase characters in your password.");
+    //Don't forget the verifyNumbers!!!!
+
+  }
+
+  // #9 Add action for password parameters (if statement???) (Also how do I combine mutltiple stings???)
+  /* The concat() method is used to join two or more strings. [w3schools]
+  This may allow me to combine whatever selected password criteria the user picked. Make sure syntax is correct!!!
+  */
+ // Variable for different characters selected to be in password
+  var password = []
+
+  if (verifySpecialCharacters) {
+    password = password.concat(specialCharacters);
+  }
+
+  if (verifyUpperCase) {
+    password = password.concat(upperCase);
+  }
+
+  if (verifyLowerCase) {
+    password = password.concat(lowerCase);
+  }
+  //Don't forget the verifyNumbers if statements!!!!
+
+  console.log(password)
+
+  // #10 Empty string to be filled based on loop selecting random characters to form the array... How??? [w3schools for loop]
+  // Variable for randomized selection of characters
+  var passwordCriteria = ""
+
+  for(var i = 0; i < verifyPasswordLength; i++) {
+    passwordCriteria = passwordCriteria + password[Math.floor(Math.random() * password.length)];
+    console.log(passwordCriteria);
+  }
+  return passwordCriteria;
+
 }
-// #9 Action for password parameters
-// #10 Empty string to be filled based on loop selecting random characters form the array
-// 
-/* 
-use window.prompt()??; NO WINDOW NEEDED DEFEATS PURPOSE OF GENERATE BTN
-if so before using window.prompt put in a var and it's name in front of the
-window prompt to catch the data.
-*/
-/*
-use example let sign = prompt("What's your sign?");
-if (sign.toLowerCase() == "scorpio") { alert("Wow! I'm a Scorpio too!");}
-*/ 
-// also look up edge cases mod.3.1.8
-
-
-// #10 Write password to the #password input
+// #11 Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
